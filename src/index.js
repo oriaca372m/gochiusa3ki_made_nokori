f2eb460b61a46114984dc60d8ebdc9f07d2a02f2
oriaca <oriaca372m@outlook.jp>
2021-03-25 23:36:49 +0900

    放送終了からの時間は日までしか表示しないように
    (動きがないほうが儚さがある気がする)

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ function generateText(now, channel, episodes) {
 	if (episodeNumber === afterLastEpisodeNumber) {
 		const lastEpisodeTime = channel.time[channel.time.length - 1].clone().add(episodeLength)
 		const diffDuration = moment.duration(now.diff(lastEpisodeTime))
-		const passedTime = diffDuration.format('Y年Mヶ月D日hh時間mm分ss.SS秒', { trim: true })
+		const passedTime = diffDuration.format('Y年Mヶ月D日', { trim: true })
 
 		return {
 			main: '放送終了',
